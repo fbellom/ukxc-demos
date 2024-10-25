33eb40ecbb1cd7a09ceefd94281797ce7e7b4034
expert <expert@mkdocs-node1.latampod.com>
2024-10-25 01:44:16 +0000

    v2024-10-24 UKnowXChange Demos

---
+++ python/app.py
@@ -0,0 +1,24 @@
+import time
+
+import redis
+from flask import Flask
+
+app = Flask(__name__)
+cache = redis.Redis(host='redis', port=6379)
+
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as e:
+            if retries == 0:
+                raise e
+            retries -= 1
+            time.sleep(0.5)
+
+
+@app.route('/')
+def index():
+    count = get_hit_count()
+    return f'Hello UKnowXChange, I have seen {count} times \n'
